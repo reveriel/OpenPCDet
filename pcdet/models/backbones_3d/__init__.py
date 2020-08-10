495ca947e47de3b0bcabe5c9c691ead2bee48a63
Guo.Xing <guoxing@mail.ustc.edu.cn>
2020-08-10 07:40:40 +0000

    add RangeVoxelBackBone4x

--- pcdet/models/backbones_3d/__init__.py
+++ pcdet/models/backbones_3d/__init__.py
@@ -1,6 +1,6 @@
 from .pointnet2_backbone import PointNet2Backbone, PointNet2MSG
 from .spconv_backbone import VoxelBackBone8x, VoxelResBackBone8x
-from .sphconv_backbone import RangeVoxelBackBone8x
+from .sphconv_backbone import RangeVoxelBackBone8x, RangeVoxelBackBone4x
 from .spconv_unet import UNetV2
 
 __all__ = {
@@ -9,5 +9,6 @@ __all__ = {
     'PointNet2Backbone': PointNet2Backbone,
     'PointNet2MSG': PointNet2MSG,
     'VoxelResBackBone8x': VoxelResBackBone8x,
-    'RangeVoxelBackBone8x': RangeVoxelBackBone8x
+    'RangeVoxelBackBone8x': RangeVoxelBackBone8x,
+    'RangeVoxelBackBone4x': RangeVoxelBackBone4x
 }
